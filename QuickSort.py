35ea1d94aa87bd8ea5e81b93684e6d07e84541d8
Jaspreet Singh <44704071+singh-jp-7@users.noreply.github.com>
2019-10-03 00:00:53 +0530

    Create QuickSort.py
    This program sorts the University Roll Numbers from the college database using the quick sort algorithm and also computes the time taken for the algorithm to sort the database.

---
+++ QuickSort.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python 3
+import mysql.connector
+import datetime
+a = datetime.datetime.now()
+def partition(arr,low,high): 
+    i = ( low-1 )     
+    pivot = arr[high]     
+  
+    for j in range(low , high): 
+  
+        if   arr[j] < pivot: 
+          
+            
+            i = i+1 
+            arr[i],arr[j] = arr[j],arr[i] 
+  
+    arr[i+1],arr[high] = arr[high],arr[i+1] 
+    return ( i+1 ) 
+  
+def quickSort(arr,low,high): 
+    if low < high: 
+  
+        
+        pi = partition(arr,low,high) 
+  
+        quickSort(arr, low, pi-1) 
+        quickSort(arr, pi+1, high)
+
+sql = "select university_roll_no from data"
+con = mysql.connector.connect(user="*****", password="*********", host="127.0.0.1", database="students")
+cursor=con.cursor()
+cursor.execute(sql)
+r_no = []
+rows = cursor.fetchall()
+for row in rows:
+    r_no.append(row[0])
+
+arr = r_no.copy()
+print("The sorted University Roll numbers from the database are as follows: \n")
+d=len(arr)-1
+quickSort(arr, 0, d)
+for i in range (len(arr)):
+    print(arr[i])
+
+b = datetime.datetime.now()
+c=b-a
+print("The time taken for the executuion is :" ,c)
